01bfe9d3c5c624293704797135ed486fc0043d8f
League <league@WTS9.attlocal.net>
2018-08-06 18:25:16 -0700

    Practice

--- src/Robots.java
+++ src/Robots.java
@@ -1,4 +1,6 @@
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -6,24 +8,63 @@ import org.jointheleague.graphical.robot.Robot;
 public class Robots {
 
 	Robot r;
-	ArrayList<Integer> List;
-	public static String robotImg;
+	ArrayList<Integer> shapes;
+	public String robotImg;
 
-	public Robots(String robotImg) {
+	public Robots(String robotImg, ArrayList<Integer>shapes) {
 
-		this.r = r;
-		this.List = List;
 		this.robotImg = robotImg;
+		this.shapes = shapes;
+		r = new Robot();
+		r.changeRobot(robotImg);
+		setRobotImg(robotImg);
+		
+	}
+
+	void getRobotImg() {
+
+	}
+
+	void setRobotImg(String robotImg) {
+		
 		r.changeRobot(robotImg);
 
 	}
-
-	void getRobotimg() {
-
+	
+	void draw() {
+		
+		for (Integer integer : shapes) {
+			
+			
+			for (int i = 0; i < integer; i++) {
+				
+				r.setPenColor(Color.black);
+				r.penDown();
+				r.setSpeed(10);
+				int x = 25;
+				r.move(x);
+				
+				
+				r.move(100);
+				r.turn(360/integer);;
+				
+			}
+			
+			
+		}
+		
 	}
-
-	void setRobotImg() {
-
+	
+	public static void main(String [] args) { 
+		
+		ArrayList<Integer> shapes = new ArrayList<Integer>();
+		 Robots rr = new Robots("https://png.pngtree.com/element_pic/00/16/07/11578277afd02ba.jpg", shapes);
+		 shapes.add(4);
+		 shapes.add(3);
+		rr.draw();
+		
+		
+		
 	}
 
 }
